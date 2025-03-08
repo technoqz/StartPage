c4f6bb338c613d2328705cc2b8374392ce127ff4
technoqz <technoqz@gmail.com>
2025-03-08 11:46:34 +0300

    show dates in RSS news

--- js/blocks/rssBlock.js
+++ js/blocks/rssBlock.js
@@ -7,13 +7,31 @@ const rssBlock = {
          }
          return text;
       };
-      return { truncate };
+
+      // Input "Sat, 08 Mar 2025 07:26:49 GMT"  -> Output: "08.03.25 07:26"
+      function convertDateFormat(dateString) {
+         // Create Date object from the input string
+         const date = new Date(dateString);
+
+         // Extract components
+         const day = String(date.getUTCDate()).padStart(2, '0');
+         const month = String(date.getUTCMonth() + 1).padStart(2, '0'); // +1 because months are 0-based
+         const year = String(date.getUTCFullYear()).slice(-2);
+         const hours = String(date.getUTCHours()).padStart(2, '0');
+         const minutes = String(date.getUTCMinutes()).padStart(2, '0');
+
+         // Return formatted string
+         return `${day}.${month}.${year} ${hours}:${minutes}`;
+      }
+
+      return { truncate, convertDateFormat };
    },
    template: `
      <div>
        <p v-if="!items.length">No RSS items available.</p>
        <div v-for="item in items" :key="item.link">
-         <a :href="item.link" target="_blank">{{ item.title }}</a>
+         <a :href="item.link" target="_blank">{{ item.title }}  <span class="block-item-date">[{{convertDateFormat(item.pubDate)}}]</span></a>
+         
          <p class="block-item-description" v-if="contentLimit > 0">
            {{ truncate(item.description, contentLimit) }}
          </p>
